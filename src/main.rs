2220dd38823927b7e525d135eb531633da17b624
Aguacero <72492241+aguacero7@users.noreply.github.com>
2024-04-23 17:51:07 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,21 @@ fn handle_client(mut stream: TcpStream) {
     let mut buffer = [0; 1024]; // Declare a mutable buffer of size 1024 to store incoming data
     stream.read(&mut buffer).unwrap(); // Read data from the stream into the buffer
 
-    // Process the request here
+
     let request = String::from_utf8_lossy(&buffer[..]); // Convert buffer to UTF-8 string, handling invalid UTF-8 sequences
     println!("Received request: {}", request); 
     let ip = stream.peer_addr().unwrap(); // Get the IP address of the client
-    // Process the request here
     println!("Connection established! {} is connected", ip);
+
     // Send response
-    let response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nHello,"; 
+    let response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nSalut !!!! \n"; 
+
     stream.write(response.as_bytes()).unwrap(); 
     stream.flush().unwrap(); 
 }
 
 fn main() { 
-    let listener = TcpListener::bind("127.0.0.1:3000").unwrap(); 
+    let listener = TcpListener::bind("10.10.0.214:3000").unwrap(); 
     println!("Listening on port 3000..."); 
 
     for stream in listener.incoming() { 
