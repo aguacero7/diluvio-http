865deaf17295b909c88be253f97b3c4bdda793f7
Aguacero <72492241+aguacero7@users.noreply.github.com>
2024-04-30 16:49:24 +0200

    PHP interpretation included with a test.php page
    Also added a link to the repo in the main page

--- src/http_server.rs
+++ src/http_server.rs
@@ -2,6 +2,8 @@ use std::io::{self, Read, Write};
 use std::net::{TcpListener, TcpStream};
 use std::process::Command;
 use std::thread;
+use std::fs;
+use std::path::Path;
 
 fn handle_client(mut stream: TcpStream) -> io::Result<()> {
     let mut buffer = [0; 1024];
@@ -15,111 +17,111 @@ fn handle_client(mut stream: TcpStream) -> io::Result<()> {
         if let Some(method) = parts.next() {
             if method == "GET" || method == "POST" {
                 if let Some(path) = parts.next() {
-                    let mut file_path = String::from("web_documents");
-                    if path == "/" {
-                        file_path.push_str("/index.html");
+                    let file_path = format!("web_documents{}", path);
+                    let file_path = if file_path.ends_with("/") {
+                        format!("{}/index.html", file_path)
                     } else {
-                        file_path.push_str(path);
-                    }
+                        file_path
+                    };
 
-                    // Check php
-                    if file_path.ends_with(".php") {
-                        let output = Command::new("php")
-                            .arg(&file_path)
-                            .output()
-                            .expect("failed to execute PHP script");
+                    if Path::new(&file_path).exists() {
+                        if file_path.ends_with(".php") {
+                            let output = Command::new("php-cgi")
+                                .arg(&file_path)
+                                .output()
+                                .expect("failed to execute PHP script");
+                        
+                            let php_response = String::from_utf8_lossy(&output.stdout);
 
-                        // proper Content-Type header for PHP script
-                        let response = format!("HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\nContent-Length: {}\r\n\r\n",
-                                               output.stdout.len());
+                            stream.write_all(b"HTTP/1.1 200 OK\r\n")?;
+                            stream.write_all(b"Content-Type: text/html; charset=UTF-8\r\n")?;
+                            stream.write_all(b"\r\n")?;
+                            stream.write_all(php_response.as_bytes())?;
+                        } else {
+                            let file_content = fs::read(&file_path)?;
 
-                        // Write the HTTP response and PHP script output to the client
-                        return stream.write_all(response.as_bytes())
-                            .and_then(|_| stream.write_all(&output.stdout))
-                            .map(|_| ());
-                    } else {
-                        let response = match std::fs::read(&file_path) {
-                            Ok(file_content) => {
-                                let content_type = if file_path.ends_with(".html") {
-                                    "text/html; charset=utf-8" // Set charset to UTF-8
-                                } else if file_path.ends_with(".css") {
-                                    "text/css"
-                                } else {
-                                    "text/plain"
-                                };
-                                format!("HTTP/1.1 200 OK\r\nContent-Type: {}\r\nContent-Length: {}\r\n\r\n{}",
-                                        content_type,
-                                        file_content.len(),
-                                        String::from_utf8_lossy(&file_content))
-                            }
-                            Err(_) => {
-                                let error_page = r#"
-                                <!DOCTYPE html>
-                                <html lang="en">
-                                <head>
-                                    <meta charset="UTF-8">
-                                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                                    <title>Error Page</title>
-                                    <style>
-                                        body {
-                                            font-family: Arial, sans-serif;
-                                            background-color: #f4f4f4;
-                                            margin: 0;
-                                            padding: 0;
-                                        }
-                                
-                                        .container {
-                                            display: flex;
-                                            justify-content: center;
-                                            align-items: center;
-                                            height: 100vh;
-                                        }
-                                
-                                        .card {
-                                            background-color: #ffffff;
-                                            padding: 20px;
-                                            border-radius: 10px;
-                                            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-                                        }
-                                
-                                        h1 {
-                                            text-align:center;
-                                            font-size: 35px;
-                                            font-weight: bold;
-                                            color: #333333;
-                                            margin-bottom: 20px;
-                                        }
-                                
-                                        p {
-                                            color: #666666;
-                                            margin-bottom: 20px;
-                                        }
-                                
-                                        .error-message {
-                                            color: #ff0000;
-                                            font-weight: bold;
-                                            margin-bottom: 20px;
-                                        }
-                                    </style>
-                                </head>
-                                <body>
-                                    <div class="container">
-                                        <div class="card">
-                                            <h1>404 Not Found</h1>
-                                            <p class="error-message">Sorry, the page you are looking for could not be found.</p>
-                                            <p>Please try again later or contact support for assistance.</p>
-                                        </div>
+                            let content_type = if file_path.ends_with(".html") {
+                                "text/html; charset=utf-8" // Set charset to UTF-8
+                            } else if file_path.ends_with(".css") {
+                                "text/css"
+                            } else {
+                                "text/plain"
+                            };
+
+                            let response = format!("HTTP/1.1 200 OK\r\nContent-Type: {}\r\nContent-Length: {}\r\n\r\n{}",
+                                                   content_type,
+                                                   file_content.len(),
+                                                   String::from_utf8_lossy(&file_content));
+
+                            stream.write_all(response.as_bytes())?;
+                        }
+                    }  else {
+                        let error_page = r#"
+                            <!DOCTYPE html>
+                            <html lang="en">
+                            <head>
+                                <meta charset="UTF-8">
+                                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                                <title>Error Page</title>
+                                <style>
+                                    body {
+                                        font-family: Arial, sans-serif;
+                                        background-color: #f4f4f4;
+                                        margin: 0;
+                                        padding: 0;
+                                    }
+
+                                    .container {
+                                        display: flex;
+                                        justify-content: center;
+                                        align-items: center;
+                                        height: 100vh;
+                                    }
+
+                                    .card {
+                                        background-color: #ffffff;
+                                        padding: 20px;
+                                        border-radius: 10px;
+                                        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+                                    }
+
+                                    h1 {
+                                        text-align:center;
+                                        font-size: 35px;
+                                        font-weight: bold;
+                                        color: #333333;
+                                        margin-bottom: 20px;
+                                    }
+
+                                    p {
+                                        color: #666666;
+                                        margin-bottom: 20px;
+                                    }
+
+                                    .error-message {
+                                        color: #ff0000;
+                                        font-weight: bold;
+                                        margin-bottom: 20px;
+                                    }
+                                </style>
+                            </head>
+                            <body>
+                                <div class="container">
+                                    <div class="card">
+                                        <h1>404 Not Found</h1>
+                                        <p class="error-message">Sorry, the page you are looking for could not be found.</p>
+                                        <p>Please try again later or contact support for assistance.</p>
                                     </div>
-                                </body>
-                                </html>
-                                "#;
-                                format!("HTTP/1.1 404 NOT FOUND\r\nContent-Type: text/html; charset=utf-8\r\nContent-Length: {}\r\n\r\n{}",
-                                        error_page.len(),
-                                        error_page)
-                            }
-                        };
+                                </div>
+                            </body>
+                            </html>
+                            "#;
 
-                        return stream.write_all(response.as_bytes()).map(|_| ());
+                        let response = format!("HTTP/1.1 404 NOT FOUND\r\nContent-Type: text/html; charset=utf-8\r\nContent-Length: {}\r\n\r\n{}",
+                                                error_page.len(),
+                                                error_page);
+
+                        stream.write_all(response.as_bytes())?;
                     }
                 }
             }
@@ -128,6 +130,8 @@ fn handle_client(mut stream: TcpStream) -> io::Result<()> {
 
     Ok(())
 }
+
+
 
 pub fn start_http_server(address: &str) -> io::Result<()> {
     let listener = TcpListener::bind(address)?;
