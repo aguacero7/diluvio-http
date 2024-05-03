a1c3caddac5855c889ba75b6200a33bb8c051b4b
Aguacero <72492241+aguacero7@users.noreply.github.com>
2024-05-03 18:43:09 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use std::env;
 #[tokio::main]
 async fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() > 1 && args[1] == "--cli" {
+    if args.len() > 0 && args[0] == "--cli" {
         cli::run().await;
     } else {
         gui::run().await.unwrap();
